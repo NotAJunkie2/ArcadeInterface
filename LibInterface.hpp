f318448272631668787782e2f701537ffd9a1dec
NotAJunkie2 <artyom.tillon@epitech.eu>
2023-03-17 20:35:16 +0100

    [UPDATE] expanded interface

--- LibInterface.hpp
+++ LibInterface.hpp
@@ -12,10 +12,25 @@
 class IDisplayModule {
     public :
         virtual ~IDisplayModule() = default;
-        virtual void init() = 0;
-        virtual void stop() = 0;
-        virtual const std::string &getName () const = 0;
-};
 
+        virtual void createWindow(size_t width, size_t height, const std::string name) = 0;
+        virtual void clearWindow() = 0;
+        virtual void updateWindow() = 0;
+        virtual void destroyWindow() = 0;
+        virtual void drawWindow(void *obj) = 0;
+
+        // Getters
+        virtual size_t getWidth() const = 0;
+        virtual size_t getHeight() const = 0;
+        virtual std::pair<size_t, size_t> getDimensions() const = 0;
+
+        // Setters
+        virtual void setWidth(size_t w) = 0;
+        virtual void setHeight(size_t h) = 0;
+        virtual void setDimensions(std::pair<size_t, size_t>) = 0;
+
+        // Load
+        virtual void loadFont() = 0;
+};
 
 #endif /* !LIBINTERFACE_HPP_ */
