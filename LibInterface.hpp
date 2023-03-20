d99985e22ac9d9d3fdecb6e02456fbb7bec6755e
NotAJunkie2 <artyom.tillon@epitech.eu>
2023-03-20 09:40:46 +0100

    [UPDATE] Added more methods to the interface

--- LibInterface.hpp
+++ LibInterface.hpp
@@ -9,10 +9,15 @@
     #define LIBINTERFACE_HPP_
     #include <iostream>
 
+class IArcadeSprite {
+
+};
+
 class IDisplayModule {
     public :
         virtual ~IDisplayModule() = default;
 
+        // Window methods
         virtual void createWindow(size_t width, size_t height, const std::string name) = 0;
         virtual void clearWindow() = 0;
         virtual void updateWindow() = 0;
@@ -30,7 +35,8 @@ class IDisplayModule {
         virtual void setDimensions(std::pair<size_t, size_t>) = 0;
 
         // Load
-        virtual void loadFont() = 0;
+        virtual void loadFont(std::string path, std::string key) = 0;
+        virtual void loadTexture(std::string path, std::string key) = 0;
 };
 
 #endif /* !LIBINTERFACE_HPP_ */
