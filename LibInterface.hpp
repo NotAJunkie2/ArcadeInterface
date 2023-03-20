0ca6989a75419a07f4c9980fac0733bf39df1438
NotAJunkie2 <artyom.tillon@epitech.eu>
2023-03-20 14:54:01 +0100

    [UPDATE] Added namespace Arcade + enum for inputs

--- LibInterface.hpp
+++ LibInterface.hpp
@@ -9,34 +9,44 @@
     #define LIBINTERFACE_HPP_
     #include <iostream>
 
-class IArcadeSprite {
+namespace Arcade {
+    enum input {
+        LEFT = 1,
+        RIGHT,
+        UP,
+        DOWN,
+        UKNOWNW = -1
+    };
 
-};
+    class IDisplayModule {
+        public :
+            virtual ~IDisplayModule() = default;
 
-class IDisplayModule {
-    public :
-        virtual ~IDisplayModule() = default;
+            // Window methods
+            virtual void createWindow(size_t width, size_t height, const std::string name) = 0;
+            virtual void clearWindow() = 0;
+            virtual void updateWindow() = 0;
+            virtual void destroyWindow() = 0;
+            virtual void drawWindow(void *obj) = 0;
 
-        // Window methods
-        virtual void createWindow(size_t width, size_t height, const std::string name) = 0;
-        virtual void clearWindow() = 0;
-        virtual void updateWindow() = 0;
-        virtual void destroyWindow() = 0;
-        virtual void drawWindow(void *obj) = 0;
+            // Getters
+            virtual size_t getWidth() const = 0;
+            virtual size_t getHeight() const = 0;
+            virtual std::pair<size_t, size_t> getDimensions() const = 0;
+            virtual bool isWindowOpen() const = 0;
 
-        // Getters
-        virtual size_t getWidth() const = 0;
-        virtual size_t getHeight() const = 0;
-        virtual std::pair<size_t, size_t> getDimensions() const = 0;
+            // Setters
+            virtual void setWidth(size_t w) = 0;
+            virtual void setHeight(size_t h) = 0;
+            virtual void setDimensions(std::pair<size_t, size_t>) = 0;
 
-        // Setters
-        virtual void setWidth(size_t w) = 0;
-        virtual void setHeight(size_t h) = 0;
-        virtual void setDimensions(std::pair<size_t, size_t>) = 0;
+            // Load
+            virtual void loadFont(std::string path, std::string key) = 0;
+            virtual void loadTexture(std::string path, std::string key) = 0;
 
-        // Load
-        virtual void loadFont(std::string path, std::string key) = 0;
-        virtual void loadTexture(std::string path, std::string key) = 0;
-};
+            // Get input
+            virtual input getInput() const = 0;
+    };
+}
 
 #endif /* !LIBINTERFACE_HPP_ */
