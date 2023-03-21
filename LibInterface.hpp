77115c1a4f1c1f0c78d5bc8b7281314207ed0c38
RayanBn <rayan.banni@epitech.eu>
2023-03-21 17:41:07 +0100

    [UPDATE] IGameModule and IDisplayModule

--- LibInterface.hpp
+++ LibInterface.hpp
@@ -15,7 +15,25 @@ namespace Arcade {
         RIGHT,
         UP,
         DOWN,
-        UKNOWNW = -1
+        SWITCH,
+        UKNOWN = -1
+    };
+
+    enum status {
+        INGAME,
+        GAMEOVER
+    };
+
+    template <typename T>
+    class IArcadeObject {
+        T _object;
+
+        public:
+            virtual ~IArcadeObject() = default;
+
+            virtual void draw(void *obj) = 0;
+            virtual void update() = 0;
+            virtual void destroy() = 0;
     };
 
     class IDisplayModule {
@@ -34,6 +52,7 @@ namespace Arcade {
             virtual size_t getHeight() const = 0;
             virtual std::pair<size_t, size_t> getDimensions() const = 0;
             virtual bool isWindowOpen() const = 0;
+            virtual bool isGraphical() const = 0;
 
             // Setters
             virtual void setWidth(size_t w) = 0;
@@ -43,9 +62,31 @@ namespace Arcade {
             // Load
             virtual void loadFont(std::string path, std::string key) = 0;
             virtual void loadTexture(std::string path, std::string key) = 0;
+            virtual void loadMap(std::string path) = 0;
 
             // Get input
             virtual input getInput() const = 0;
+    };
+
+    class IGameModule {
+        public:
+            virtual ~IGameModule() = default;
+
+            virtual void startTimer() = 0;
+            virtual void treatInput(input key) = 0;
+
+            // Getters
+            virtual int getScore() const = 0;
+            virtual std::pair<size_t, size_t> getPlayerPos() const = 0;
+            virtual status getGameStatus() const = 0;
+
+            // Setters
+            virtual void incScore(int increment = 1) = 0;
+            virtual void decScore(int decrement = 1) = 0;
+            virtual void setScore(int score) = 0;
+            virtual void setPlayerPos(std::pair<size_t, size_t> pos) = 0;
+            virtual void setEnemiesPos(std::pair<size_t, size_t> pos) = 0;
+            virtual void setGameStatus(status gameStatus) const = 0;
     };
 }
 
